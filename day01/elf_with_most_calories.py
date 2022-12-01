f98b17c14709050137050ebd089cb83f9d3fab40
DavidS <davids@digitalcabinet.co.za>
2022-12-01 20:23:48 +0100

    Add submission for day01 - part2

--- day01/elf_with_most_calories.py
+++ day01/elf_with_most_calories.py
@@ -12,3 +12,13 @@ with open('calories_list.txt') as f:
     max_calories = max(elf_calories)
     elf_index = elf_calories.index(max_calories)
     print (f"Elf number {elf_index+1} has {max_calories} calories.")
+
+    # part 2
+    # convert list to dictionary: 
+    elf_calories_dict = dict(enumerate(elf_calories))
+    #https://stackoverflow.com/questions/16772071/sort-dict-by-value-python/16772088#16772088
+    reverse_sorted = sorted(elf_calories_dict.items(), key=lambda x:x[1], reverse=True)
+    #print(reverse_sorted)
+    top3 = list(reverse_sorted)[:3]
+    for elf_num, calories in top3:
+        print (f"Elf number {elf_num+1} has {calories} calories.")
