15169a357b14647d8f29989f9893b5c9d1bbbedd
DavidS <davids@digitalcabinet.co.za>
2022-12-06 10:54:19 +0100

    Add submission for day06 - part2

--- day06/tuning_trouble.py
+++ day06/tuning_trouble.py
@@ -2,6 +2,7 @@ with open("datastream.txt", "r") as f:
     for line in f.readlines():
         c_pos, start = 0, 0
         memo = []
+        packet_found = False
         for c in line:
             c_pos += 1
             if c in memo:
@@ -9,5 +10,10 @@ with open("datastream.txt", "r") as f:
                 memo = memo[memo_pos_of_c + 1 :]
             memo.append(c)
             if len(memo) == 4:
-                print(c_pos)
+                if not packet_found:
+                    packet_found = True
+                    print(f"Start of packet: {c_pos}")
+                    # break
+            if len(memo) == 14:
+                print(f"Start of message: {c_pos}")
                 break
