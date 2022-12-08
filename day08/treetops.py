022038e5929682aa71f65b47893d8731c6a89b83
DavidS <davids@digitalcabinet.co.za>
2022-12-08 22:41:42 +0100

    Adds minor updates for part1

--- day08/treetops.py
+++ day08/treetops.py
@@ -9,19 +9,20 @@ MT = list(map(list, zip(*M)))  # transpose matrix
 
 
 def checkVisibilityInRow(M):
-    visible_trees = []
+    visible_trees = {}
     y_pos = 0
     for row in M[1 : (ROWS - 1)]:  # excluding edges
         y_pos += 1
         x_pos = 0
         for n in row[1 : (COLS - 1)]:  # excluding edges
             x_pos += 1
+            coords = (x_pos, y_pos)
             if all(i < n for i in row[:x_pos]):
                 # print(f"{n} IS VISIBLE FROM THE LEFT")
-                visible_trees.append([x_pos, y_pos])
+                visible_trees[coords] = n
             elif all(i < n for i in row[1 + x_pos :]):
                 # print(f"{n} IS VISIBLE FROM THE RIGHT")
-                visible_trees.append([x_pos, y_pos])
+                visible_trees[coords] = n
             # else:
             # print(f"{n} IS NOT VISIBLE FROM THE LEFT OR RIGHT")
     return visible_trees
@@ -30,9 +31,12 @@ def checkVisibilityInRow(M):
 vis_M = checkVisibilityInRow(M)
 vis_MT = checkVisibilityInRow(MT)
 vis_edges = 2 * ROWS + 2 * COLS - 4
+
+# count duplicates
 dups = 0
 for i in vis_MT:
-    if list([i[1], i[0]]) in vis_M:
+    iT = (i[1], i[0])
+    if iT in vis_M:
         dups += 1
 
-print(len(vis_M) + len(vis_MT) - dups + vis_edges)
+print(f"Tree Visibility: {len(vis_M) + len(vis_MT) - dups + vis_edges}")
