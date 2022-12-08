fd5e6569953e932909b457ac0df3c3e587181b72
DavidS <davids@digitalcabinet.co.za>
2022-12-09 00:16:19 +0100

    Add submission for day08 - part2

--- day08/treetops.py
+++ day08/treetops.py
@@ -1,3 +1,6 @@
+# This solution uses a matrix (2D list) as well as its transpose.
+# the rows of each are processed, and the results of each are combined,
+# taking into account the need to transpose indices and remove duplicates.
 M = []
 with open("treemap.txt", "r") as f:
     for line in f:
@@ -5,10 +8,10 @@ with open("treemap.txt", "r") as f:
         M.append(row)
 
 ROWS, COLS = len(M), len(M[1])
-MT = list(map(list, zip(*M)))  # transpose matrix
+MT = list(map(list, zip(*M)))  # transpose matrix (https://stackoverflow.com/a/6473724)
 
 
-def checkVisibilityInRow(M):
+def checkVisibilityInRows(M):
     visible_trees = {}
     y_pos = 0
     for row in M[1 : (ROWS - 1)]:  # excluding edges
@@ -28,8 +31,32 @@ def checkVisibilityInRow(M):
     return visible_trees
 
 
-vis_M = checkVisibilityInRow(M)
-vis_MT = checkVisibilityInRow(MT)
+def calcScenicScoreInRows(M):
+    scenic_scores = {}
+    y_pos = 0
+    for row in M:
+        y_pos += 1
+        x_pos = 0
+        for n in row:
+            x_pos += 1
+            coords = (x_pos, y_pos)
+            scenic_score_left = 0
+            scenic_score_right = 0
+            for i in reversed(row[: x_pos - 1]):  # look left
+                scenic_score_left += 1
+                if i >= n:
+                    break
+            for i in row[x_pos:]:  # look right
+                scenic_score_right += 1
+                if i >= n:
+                    break
+            scenic_scores[coords] = scenic_score_left * scenic_score_right
+
+    return scenic_scores
+
+
+vis_M = checkVisibilityInRows(M)
+vis_MT = checkVisibilityInRows(MT)
 vis_edges = 2 * ROWS + 2 * COLS - 4
 
 # count duplicates
@@ -39,4 +66,15 @@ for i in vis_MT:
     if iT in vis_M:
         dups += 1
 
+score_M = calcScenicScoreInRows(M)
+score_MT = calcScenicScoreInRows(MT)
+
+# calculate scenic_score
+max_scenic_score = 0
+for i in score_MT:
+    iT = (i[1], i[0])
+    tree_scenic_score = score_M[i] * score_MT[iT]
+    max_scenic_score = max(max_scenic_score, tree_scenic_score)
+
 print(f"Tree Visibility: {len(vis_M) + len(vis_MT) - dups + vis_edges}")
+print(f"Scenic Score: {max_scenic_score}")
